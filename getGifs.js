a773dfeb8f8c36861575824a12954ebd29ec56e4
Ian Walston <ikwalston@comcast.net>
2018-12-13 17:56:06 -0500

    seperated main js file from accessory js file. polishing code

---
+++ getGifs.js
@@ -0,0 +1,96 @@
+const apiKey = "P1kFSxY5ndG957IWKgEycqXpTX7rptME"
+var limit = 10
+
+// define getGifs Function
+var getGifs = (word) => {
+
+    $("#loadingDiv").text("waiting for responses...")
+
+    //getting gueryURL. currently searches for dogs
+    var queryURL = `https://api.giphy.com/v1/gifs/search?q=${word}&api_key=${apiKey}&limit=${limit}`;
+
+    //this object will count up all labels in the google vision api response
+    var visionCount = {};
+
+    //making an ajax call to giphy
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        //iterating through each gif object in the giphy response
+        response.data.forEach((data) => {
+        
+            //defining a variable for this image that will later be used by an inner function called 'gifPausePlay'
+            var that = this
+
+            //making a div with image and gif title, rating
+            var div = $("<div>")
+            var gif = $("<img>")
+            gif.attr("src", data.images.original_still.url)
+            gif.on("click", (that) => gifPausePlay(that))
+            div.append(gif)
+            div.append($("<h3>").text(data.title))
+            var row = $("<div class='row'>")
+            row.append($("<p class='col-md-11'>").text(`Rating: ${data.rating}`))
+            row.append($("<a class='col-md-1'>").text("Source").attr("href", data.source))
+            div.append(row)
+
+            //making a call to the Google Vision Api
+            //this alternative to an ajax call from https://stackoverflow.com/users/4891910/a-moore
+            var visionApiOptions = JSON.stringify({ "requests": [{ "image": { "source": { "imageUri": data.images.original_still.url } }, "features": [{ "type": "LABEL_DETECTION", "maxResults": 5 }] }] });
+            var visionApiRequest = new XMLHttpRequest;
+            //  visionApiRequest.onload = function () { console.log(visionApiRequest.responseText) };
+            visionApiRequest.open("POST", "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyA9EHeI2lrYJnjfEMhI0rU-J8yyfAOSOAs", !0);
+            visionApiRequest.send(visionApiOptions)
+            //end of code from https://stackoverflow.com/users/4891910/a-moore
+
+
+            //making a div with information from google vision
+            visionApiRequest.onload = function () {
+                var visionDiv = $("<div>")
+                const vision = JSON.parse(visionApiRequest.response)
+                visionDiv.append($("<b>").text("Google Vision Annotation:"))
+
+                //iterating through each vision response for a single gif
+                vision.responses.forEach((response) => {
+                    response.labelAnnotations.forEach((annotation) => {
+                        
+                        //write percentage from proportion provided by the Vision response
+                        visionDiv.append(`(${annotation.score.toFixed(2) * 100}%)${annotation.description}, `)
+           
+                        //start count of a label(description) from Vision response if its the first time foun 
+                        if (!visionCount[annotation.description]) {visionCount[annotation.description] = 1}
+                        //count occurences of labels from Vision response
+                        visionCount[annotation.description]++
+                    })
+                })
+
+                //add vision information to the new div. Add the new div to the feed in the document. 
+                $(div).append(visionDiv)
+                $("#gifsDiv").prepend(div)
+                $("#gifsDiv").prepend("<hr>")
+
+                //create the report of the label count from vision responses from all GIFs in this GET
+                var visSumDiv = $("<div>")
+                visSumDiv.append($("<h3>").text(`Total Labels`))
+                visSumDiv.append($("<p>").text(`From all ${limit} GIFs, here is how many times each label was found in the Google Vision annotation.`))
+                Object.keys(visionCount).forEach((key) => {
+                    // visSumDiv.append($("<p>").text(`${key}: ${visionCount[key]}`))
+                    visSumDiv.append(`${visionCount[key]}: ${key}<br>`)
+                })
+                $("#visionSummeryDiv").html(visSumDiv)
+                $("#loadingDiv").empty()
+            }
+        })
+    })
+    //this makes a button to get 256 gifs of the current topic
+    var btn = $("<button>")
+    btn.addClass("btn btn-primary btn3 btn-more")
+    btn.text("Get More Gifs!")
+    btn.on("click", () => {
+        limit = 256
+        getGifs(word)
+    })
+    $("#feedEndDiv").html(btn)
+}
--- script.js
+++ script.js
@@ -1,104 +1,7 @@
+//defining global variables
 const array = ["It's Always Sunny In Philidelphia", "Bojack Horseman", "30 Rock", "Blackish", "Parks and Rec", "The Eric Andre Show", "My little Pony: Friendship is Magic", "Spongebob Squarepants", "King of the Hill", "Insecure", "Broad City"]
-var limit = 10
-const apiKey = "P1kFSxY5ndG957IWKgEycqXpTX7rptME"
 
-// define getGifs Function
-var getGifs = (word) => {
-    //getting gueryURL. currently searches for dogs
-    var queryURL = `https://api.giphy.com/v1/gifs/search?q=${word}&api_key=${apiKey}&limit=${limit}`;
-
-    //making an ajax call 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-
-        var visionCount = {};
-
-
-        response.data.forEach((data) => {
-            //defining a variable for this image that will later be used by an inner function called 'gifPausePlay'
-            var that = this
-
-            //making a div with image and gif title, rating
-            var div = $("<div>")
-            var gif = $("<img>")
-            gif.attr("src", data.images.original_still.url)
-            gif.on("click", (that) => gifPausePlay(that))
-            div.append(gif)
-            div.append($("<h3>").text(data.title))
-
-
-            var row = $("<div class='row'>")
-
-            row.append($("<p class='col-md-6'>").text(`Rating: ${data.rating}`))
-            row.append($("<a class='col-md-6'>").text("Source").attr("href", data.source))
-
-            div.append(row)
-
-            //this alternative to an ajax call from https://stackoverflow.com/users/4891910/a-moore
-            var b = JSON.stringify({ "requests": [{ "image": { "source": { "imageUri": data.images.original_still.url } }, "features": [{ "type": "LABEL_DETECTION", "maxResults": 5 }] }] });
-            var e = new XMLHttpRequest;
-            //  e.onload = function () { console.log(e.responseText) };
-            e.open("POST", "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyA9EHeI2lrYJnjfEMhI0rU-J8yyfAOSOAs", !0);
-            e.send(b)
-            //end of code from https://stackoverflow.com/users/4891910/a-moore
-
-
-            //making a div with information from google vision
-
-            e.onload = function () {
-
-                var visionDiv = $("<div>")
-                const vision = JSON.parse(e.response)
-                visionDiv.append($("<b>").text("Google Vision Annotation:"))
-
-                vision.responses.forEach((response) => {
-                    response.labelAnnotations.forEach((annotation) => {
-
-                        //count occurences of descriptions 
-                        if (!visionCount[annotation.description]) {
-                            visionCount[annotation.description] = 1
-                        }
-                        visionCount[annotation.description]++
-
-                        visionDiv.append(`(${annotation.score.toFixed(2) * 100}%)${annotation.description}, `)
-                    })
-                })
-                $(div).append(visionDiv)
-                $("#gifsDiv").prepend(div)
-                $("#gifsDiv").prepend("<hr>")
-
-                var visSumDiv = $("<div>")
-
-                visSumDiv.append($("<h3>").text(`Total Descriptions`))
-
-                visSumDiv.append($("<p>").text(`Out of the ${limit} GIFs, here is how many times each description was found by Google Vision.`))
-
-                Object.keys(visionCount).forEach((key) => {
-                    visSumDiv.append($("<p>").text(`${key}: ${visionCount[key]}`))
-
-                    console.log(visSumDiv)
-                })
-
-                $("#visionSummeryDiv").html(visSumDiv)
-
-                // $("#visionSummeryDiv").text(JSON.stringify(visionCount).replace(/[{}"]/g, "").replace(/,/g, ", "))
-            }
-        })
-        //this gets 256 gifs from the current topic
-        var btn = $("<button>")
-        btn.addClass("btn btn-primary butn")
-        btn.text("Get More Gifs")
-        btn.on("click", () => {
-            limit = 256
-            getGifs(word)
-        })
-        $("#gifsDiv").append(btn)
-    })
-}
-
-//on click function that switches between playing and paused gif
+//on click function that switches between playing and paused gif. I was having a problem with (this), so I used (that). Uses regex to put a "_s" on or off the git title. 
 var gifPausePlay = (that) => {
     img = $(that.currentTarget)
     if (img.hasClass("playing")) {
@@ -111,7 +14,7 @@ var gifPausePlay = (that) => {
     }
 }
 
-//make a button in the buttonsDiv for each item in the array
+//defining a function that will make a button in the buttonsDiv for each item in the array
 var makeButtons = () => {
     $("#buttonsDiv").empty()
     array.forEach((item) => {
@@ -132,13 +35,14 @@ var makeButtons = () => {
     })
 }
 
-//adding functionallity to button adding form
+//this makes the add topic button add the user input to the array of topics, and getGifs of topic
 $("#add-the-topic").on("click", () => {
     event.preventDefault();
     userInput = $("#topic-input").val()
     array.unshift(userInput)
+    getGifs(userInput)
     makeButtons()
 })
 
-//making buttons for topics in initial array
+//making buttons for default topics
 makeButtons()
